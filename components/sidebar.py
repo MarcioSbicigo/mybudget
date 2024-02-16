3db0ecd2afd192c3fd96c7e38ed007686e88746a
Marcio Junior Sbicigo <marciosbicigo@hotmail.com>
2024-02-16 15:53:13 -0300

    add sidebar

--- components/sidebar.py
+++ components/sidebar.py
@@ -10,18 +10,39 @@ import plotly.express as px
 import numpy as np
 import pandas as pd
 
-
-
-
-
-
 # ========= Layout ========= #
 layout = dbc.Col([
+                html.H1("MyBudget", className='text-primary'),
+                html.P("by ASIMOV", className='text-info'),
+                html.Hr(),
+    
+    # Seção de Perfil
+                dbc.Button(id='botao_avatar',
+                           children=[html.Img(src='/assets/img_hom.png', id='avatar_change', alt='Avatar', className='perfil_avatar')
+                                     ], style={'background-color': 'transparent', 'border-color': 'transparent'}),
+    
+    # Seção Lançamento de Receitas/Despesas
+                dbc.Row([
+                    dbc.Col([dbc.Button(color='success', id='open-novo-receita', children=['+ Receita'])], width=6),
+                    dbc.Col([dbc.Button(color='danger', id='open-novo-despesa', children=['- Despesa'])], width=6)
+                ]),
                 
-            ])
+    # Seção de Navegação
+                html.Hr(),
+                
+                dbc.Nav(
+                    [
+                        dbc.NavLink("Dashboard", href="/dashboards", active="exact"),
+                        dbc.NavLink("Extratos", href="/extratos", active="exact"),
+                    ], vertical=True, pills=True, id='nav_buttons', style={"margin-bottom": "50px"}),
+                
+                #ThemeChangerAIO(aio_id="theme", radio_props={"value":dbc.themes.QUARTZ})
+    
+            
+], id='sidebar_completa')
 
-
-
+    
+    
 
 
 # =========  Callbacks  =========== #
--- myindex.py
+++ myindex.py
@@ -6,24 +6,41 @@ import pandas as pd
 import plotly.express as px
 
 from app import *
-
+from components import dashboards, extratos, sidebar
 
 
 
 # =========  Layout  =========== #
 content = html.Div(id="page-content")
 
-
 app.layout = dbc.Container(children=[
+    # dcc.Store(id='store-receitas', data=df_receitas_aux),
+    # dcc.Store(id="store-despesas", data=df_despesas_aux),
+    # dcc.Store(id='stored-cat-receitas', data=list_receitas_aux),
+    # dcc.Store(id='stored-cat-despesas', data=list_despesas_aux),
+    
+    dbc.Row([
+        dbc.Col([
+            dcc.Location(id="url"),
+            sidebar.layout
+        ], md=2),
+
+        dbc.Col([
+            html.Div(id="page-content")
+        ], md=10),
+    ])
+
+], fluid=True, style={"padding": "0px"}, className="dbc")
 
 
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/" or pathname == "/dashboards":
+        return dashboards.layout
 
-
-
-], fluid=True,)
-
-
-
+    if pathname == "/extratos":
+        return extratos.layout
+        
 
 if __name__ == '__main__':
-    app.run_server(port=8051, debug=True)
+    app.run_server(debug=True)
