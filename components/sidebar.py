659f9a57228cd52189d9d6d303f04f634737e880
Marcio Junior Sbicigo <marciosbicigo@hotmail.com>
2024-02-18 15:49:56 -0300

    update modals

--- components/sidebar.py
+++ components/sidebar.py
@@ -57,27 +57,24 @@ layout = dbc.Col([
             
 ], id='sidebar_completa')
 
-    
-    
-
-
 # =========  Callbacks  =========== #
 # Pop-up receita
 @app.callback(
-    Output('modal-novo-receita', 'is_open'),
-    Input('open-novo-receita', 'n_click'),
-    State('modal-novo-receita', 'is_open')
+    Output("modal-novo-receita", "is_open"),
+    Input("open-novo-receita", "n_clicks"),
+    State("modal-novo-receita", "is_open")
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
+
+# Pop-up despesa
+@app.callback(
+    Output("modal-novo-despesa", "is_open"),
+    Input("open-novo-despesa", "n_clicks"),
+    State("modal-novo-despesa", "is_open")
 )
 def toggle_modal(n1, is_open):
     if n1:
         return not is_open
     
-# Pop-up despesa
-@app.callback(
-    Output('modal-novo-despesa', 'is_open'),
-    Input('open-novo-despesa', 'n_click'),
-    State('modal-novo-despesa', 'is_open')
-)
-def toggle_modal(n1, is_open):
-    if n1:
-        return not is_open
