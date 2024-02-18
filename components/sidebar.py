5a00d1c6fac0da75a849c3a990646277ace3b252
Marcio Junior Sbicigo <marciosbicigo@hotmail.com>
2024-02-18 15:31:39 -0300

    Add modals

--- components/sidebar.py
+++ components/sidebar.py
@@ -27,6 +27,22 @@ layout = dbc.Col([
                     dbc.Col([dbc.Button(color='danger', id='open-novo-despesa', children=['- Despesa'])], width=6)
                 ]),
                 
+                # Modal Receita
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar receita')),
+                    dbc.ModalBody([
+                        
+                    ])
+                ], id='modal-novo-receita'),
+                
+                # Modal Despesa
+                dbc.Modal([
+                    dbc.ModalHeader(dbc.ModalTitle('Adicionar despesa')),
+                    dbc.ModalBody([
+                        
+                    ])
+                ], id='modal-novo-despesa'),
+                                
     # Seção de Navegação
                 html.Hr(),
                 
@@ -47,3 +63,21 @@ layout = dbc.Col([
 
 # =========  Callbacks  =========== #
 # Pop-up receita
+@app.callback(
+    Output('modal-novo-receita', 'is_open'),
+    Input('open-novo-receita', 'n_click'),
+    State('modal-novo-receita', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
+    
+# Pop-up despesa
+@app.callback(
+    Output('modal-novo-despesa', 'is_open'),
+    Input('open-novo-despesa', 'n_click'),
+    State('modal-novo-despesa', 'is_open')
+)
+def toggle_modal(n1, is_open):
+    if n1:
+        return not is_open
